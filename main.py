f909822c59d404313ebacdfc520bcc249d07ca12
jiangych1 <jiangych1@shanghaitech.edu.cn>
2022-05-25 08:10:13 +0800

    beta_upd

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    f1 = wheat_field.field()
+    # f1 = wheat_field.field()
+    # while f1.go_next_wheat():
+    #     pass
 
-    while f1.go_next_wheat():
+    fb1 = wheat_field.beta_field()
+    while fb1.go_next_wheat():
         pass
-
-    f1.debug_normalize()
+    fb1.debug_normalize()
 
 
--- wheat_field.py
+++ wheat_field.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 
 class field():
     def __init__(self, N = 1000):
@@ -8,12 +9,19 @@ class field():
         self.k = 0
         self.wheat_record = []
 
+        self.__finished = False
+
         self.__mean = random.uniform(0, 500)
         self.__std = random.uniform(0.1, 4)
-        self.__finished = False
 
     def __gen_wheat(self):
         return np.random.normal(self.__mean, self.__std)
+
+    def __reward_normalize(self, reward):
+        return (reward - self.__mean) / self.__std
+
+    def __check_info(self, normazlized_R):
+        return (normazlized_R, self.__mean, self.__std)
 
     def go_next_wheat(self):
         if self.__finished == True:
@@ -32,12 +40,6 @@ class field():
     def is_finished(self):
         return self.__finished
 
-    def __reward_normalize(self, reward):
-        return (reward - self.__mean) / self.__std
-
-    def __check_info(self, normazlized_R):
-        return (normazlized_R, self.__mean, self.__std)
-
     def reward_check(self):
         if self.k == 0:
             print("You haven't ever entered the field!")
@@ -54,5 +56,56 @@ class field():
         plt.title('normalized_distribution')
         plt.show()
 
+class beta_field():
+    def __init__(self, N=1000):
+        self.N = N
+        self.k = 0
+        self.wheat_record = []
 
+        self.__finished = False
 
+        self.__scale = random.uniform(10, 500)
+        self.__alpha = random.uniform(1, 100)
+        self.__beta = random.uniform(1, 100)
+
+    def __gen_wheat(self):
+        return stats.beta.rvs(self.__alpha, self.__beta) # * self.__scale
+
+    def __reward_normalize(self, reward):
+        return reward
+
+    def __check_info(self, normazlized_R):
+        return normazlized_R
+
+    def go_next_wheat(self):
+        if self.__finished == True:
+            print("You have made your choice!")
+            return None
+        elif self.k == self.N:
+            self.__finished = True
+            print("You reach the end of field.")
+            return None
+        else:
+            new_wheat = self.__gen_wheat()
+            self.wheat_record.append(new_wheat)
+            self.k += 1
+            return new_wheat
+
+    def is_finished(self):
+        return self.__finished
+
+    def reward_check(self):
+        if self.k == 0:
+            print("You haven't ever entered the field!")
+            return None
+
+        self.__finished = True
+
+        choosen = self.wheat_record[self.k - 1]
+        normazlized_res = self.__reward_normalize(choosen)
+        return self.__check_info(normazlized_res)
+
+    def debug_normalize(self):
+        plt.hist(self.__reward_normalize(np.array(self.wheat_record)), bins=10, rwidth=0.8, density=True)
+        plt.title('Beta({0},{1})'.format(round(self.__alpha, 2), round(self.__beta, 2)))
+        plt.show()
