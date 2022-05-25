854c98e6a9c8e41e69472935839b868824aef5cd
jiangych1 <jiangych1@shanghaitech.edu.cn>
2022-05-25 10:29:20 +0800

    beta_upd

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    # f1 = wheat_field.field()
+    # f1 = wheat_field.normal_field()
     # while f1.go_next_wheat():
     #     pass
+    # f1.debug_hist()
 
     fb1 = wheat_field.beta_field()
     while fb1.go_next_wheat():
         pass
-    fb1.debug_normalize()
+    fb1.debug_hist()
 
 
