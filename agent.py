8db0bb246cbc0d39977cd0b1a31d8376a11feb5d
zhuhr <zhuhr@shanghaitech.edu.cn>
2022-05-27 10:23:03 +0800

    chr

--- agent.py
+++ agent.py
@@ -1,7 +1,7 @@
-import numpy as np
-
 import wheat_field
 import random
+import numpy as np  # new import
+from scipy.stats import norm        # new import
 
 class Agent():
     def __init__(self):
@@ -26,6 +26,16 @@ class Agent():
         self.reward_info = field.finish_and_check()
         self.learning()
         return self.reward_info
+
+class Agent_random(Agent):
+    def to_contiune(self, field):
+        if len(field.wheat_record) == 0:
+            return True
+        random_number = random.uniform(0, 1)
+        if random_number <= 0.5:
+            return False
+        else:
+            return True
 
 class Agent_37(Agent):
     def to_contiune(self, field):
@@ -55,29 +65,71 @@ class Agent_37_t3(Agent):
         else:
             return True
 
+
 class Agent_sqrt_n(Agent):
     def to_contiune(self, field):
-        if field.k < np.sqrt(field.N):
+        if field.k < field.N**0.5:
             return True
         if field.height_of_this_wheat() == max(field.wheat_record):
             return False
         else:
             return True
 
-class Agent_sqrt_n_t2(Agent):
-    def __init__(self):
-        Agent.__init__(self)
-
-        self.t2_rank = [float('-inf'), float('-inf')]
-
+class Agent_prob_decision(Agent):
     def to_contiune(self, field):
-        if field.k < np.sqrt(field.N):
-            if field.height_of_this_wheat() > self.t2_rank[0]:
-                self.t2_rank[0] = field.height_of_this_wheat()
-                self.t2_rank.sort()
+        if len(field.wheat_record) == 0:
+            return True
+        wheat_list = np.array(field.wheat_record)
+        # use MLE to perdict normal distribution parameters with before and current samples
+        mu = np.mean(wheat_list)
+        sigma = np.var(wheat_list)
+        not_larger_prob = (norm.cdf(wheat_list[-1], mu, sigma))**(field.N - len(wheat_list))
+        if not_larger_prob >= 0.5:
+            return False
+        else:
             return True
 
-        if field.height_of_this_wheat() > self.t2_rank[0]:
+class Agent_prob(Agent):
+    def to_contiune(self, field):   # to contiune 写错
+        if len(field.wheat_record) == 0:
+            return True
+        wheat_list = np.array(field.wheat_record)
+        # use MLE to perdict normal distribution parameters with before and current samples
+        mu = np.mean(wheat_list)
+        sigma = np.var(wheat_list)
+        not_larger_prob = (norm.cdf(wheat_list[-1], mu, sigma))**(field.N - len(wheat_list))
+        random_number = random.uniform(0, 1)
+        if random_number <= not_larger_prob:
+            return False
+        else:
+            return True
+
+class Agent_prob_decision_37(Agent):
+    def to_contiune(self, field):
+        if field.compute_explore_rate() < 0.37:
+            return True
+        if len(field.wheat_record) == 0:
+            return True
+        wheat_list = np.array(field.wheat_record)
+        # use MLE to perdict normal distribution parameters with before and current samples
+        mu = np.mean(wheat_list)
+        sigma = np.var(wheat_list)
+        not_larger_prob = (norm.cdf(wheat_list[-1], mu, sigma))**(field.N - len(wheat_list))
+        if not_larger_prob >= 0.5:
+            return False
+        else:
+            return True
+
+class Agent_prob_decision_former(Agent):
+    def to_contiune(self, field):
+        if len(field.wheat_record) == 0:
+            return True
+        wheat_list = np.array(field.wheat_record)
+        # use MLE to perdict normal distribution parameters with before and current samples
+        mu = np.mean(wheat_list)
+        sigma = np.var(wheat_list)
+        temp_prob = norm.cdf(wheat_list[-1], mu, sigma)
+        if temp_prob >= 0.9:
             return False
         else:
             return True
@@ -89,6 +141,5 @@ model_Field.wheat_record
 model_Field.compute_explore_rate()
 model_Field.height_of_this_wheat()
 """
-
 
 
--- main.py
+++ main.py
@@ -8,18 +8,22 @@ if __name__ == '__main__':
     nf1 = wheat_field.Normal_Field()
     # agent_test.debug_field(nf1)
 
+    ag_random = agent.Agent_random()
     ag_37 = agent.Agent_37()
     ag_37_t3 = agent.Agent_37_t3()
     ag_sqrt_n = agent.Agent_sqrt_n()
-    ag_sqrt_n_t2 = agent.Agent_sqrt_n_t2()
+    ag_prob_decision = agent.Agent_prob_decision()
+    ag_prob = agent.Agent_prob()
+    ag_prob_decision_37 = agent.Agent_prob_decision_37()
+    ag_prob_decision_former = agent.Agent_prob_decision_former()
 
     num_game = 100
 
+    print("ag_random,avg=", agent_test.normalFieldTest_avg(num_game,ag_random))
     print("ag_37,avg=", agent_test.normalFieldTest_avg(num_game,ag_37))
     print("ag_37_t3,avg=", agent_test.normalFieldTest_avg(num_game, ag_37_t3))
     print("ag_sqrt_n,avg=", agent_test.normalFieldTest_avg(num_game, ag_sqrt_n))
-    print("ag_sqrt_n_t2,avg=", agent_test.normalFieldTest_avg(num_game, ag_sqrt_n_t2))
-
-
-
-
+    print("ag_prob_decision,avg=", agent_test.normalFieldTest_avg(num_game, ag_prob_decision))
+    print("ag_prob,avg=", agent_test.normalFieldTest_avg(num_game, ag_prob))
+    print("ag_prob_decision_37,avg=", agent_test.normalFieldTest_avg(num_game, ag_prob_decision_37))
+    print("ag_prob_decision_former,avg=", agent_test.normalFieldTest_avg(num_game, ag_prob_decision_former))
