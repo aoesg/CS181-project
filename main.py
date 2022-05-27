0cb2d63f38a5477d9bb9028db3b189c294f39d2b
jiangych1 <jiangych1@shanghaitech.edu.cn>
2022-05-27 09:36:40 +0800

    t2_upd

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ if __name__ == '__main__':
     ag_sqrt_n = agent.Agent_sqrt_n()
     ag_sqrt_n_t2 = agent.Agent_sqrt_n_t2()
 
-    print("ag_37,avg=", agent_test.normalFieldTest_avg(100,ag_37))
-    print("ag_37_t3,avg=", agent_test.normalFieldTest_avg(100, ag_37_t3))
-    print("ag_sqrt_n,avg=", agent_test.normalFieldTest_avg(100, ag_sqrt_n))
-    print("ag_sqrt_n_t2,avg=", agent_test.normalFieldTest_avg(100, ag_sqrt_n_t2))
+    num_game = 100
+
+    print("ag_37,avg=", agent_test.normalFieldTest_avg(num_game,ag_37))
+    print("ag_37_t3,avg=", agent_test.normalFieldTest_avg(num_game, ag_37_t3))
+    print("ag_sqrt_n,avg=", agent_test.normalFieldTest_avg(num_game, ag_sqrt_n))
+    print("ag_sqrt_n_t2,avg=", agent_test.normalFieldTest_avg(num_game, ag_sqrt_n_t2))
 
 
 
